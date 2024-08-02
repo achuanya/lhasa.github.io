323989b79676008d123222e0f5c0e36b7b7721ac
achuanya <haibao1027@gmail.com>
2024-08-02 18:05:29 +0800

    update

--- api/GrabLatestRSS/main.go
+++ api/GrabLatestRSS/main.go
@@ -231,6 +231,7 @@ func fetchRSS(config Config, feeds []string) ([]Article, error) {
 
 	// 截断
 	nameMapping := map[string]string{
+		"obaby@mars": "obaby",
 		"青山小站 | 一个在帝都搬砖的新时代农民工":       "青山小站",
 		"Homepage on Miao Yu | 于淼":    "于淼",
 		"Homepage on Yihui Xie | 谢益辉": "谢益辉",
